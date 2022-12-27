c7e7c83f8fe4873d65e22b25275413af3559bdfd
bertfarina <bertfarina@gmail.com>
2022-12-27 16:39:00 +0000

    Launch Ball
    Lauch the ball dissabling the spring joint 2D after releasing

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,8 +5,11 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint2D;
+    [SerializeField] private float delayToFree;
     
     private Camera mainCamera;
+    private bool isDragging;
 
     private void Start()
     {
@@ -15,17 +18,39 @@ public class BallHandler : MonoBehaviour
 
     private void Update()
     {
+        if (currentBallRigidbody == null) return;
+        
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                LauchBall();
+            }
+            
+            isDragging = false;
             return;
         }
-        
+
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
         
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
         currentBallRigidbody.position = worldPosition;
+    }
+
+    private void LauchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+        
+        Invoke(nameof(DetachBall), delayToFree);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint2D.enabled = false;
+        currentBallSpringJoint2D = null;
     }
 }
